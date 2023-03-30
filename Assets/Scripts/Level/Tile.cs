5fe802a8e4e81162f2b9e76b1083bb7c5b355b49
Pavel Solomatin <crave_ozer_man@mail.ru>
2023-03-31 01:54:59 +0300

    VR-1 Add classes' architecture

---
+++ Assets/Scripts/Level/Tile.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Level
+{
+    public enum TileColor : int 
+    {
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+        Yellow = 3,
+    }
+
+    public enum TileType : int
+    {
+        Border = 0,
+        Open = 1,
+        Blocked = 2,
+    }
+
+    public class Tile : MonoBehaviour
+    {
+        public FieldController fieldController;
+        public TileColor tileColor;
+        public TileType tileType;
+        public Vector2 position;
+        public bool selected;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
