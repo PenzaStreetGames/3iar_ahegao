9968ba10e9122707ac7f1f302fc10342d1a45a49
Pavel Solomatin <crave_ozer_man@mail.ru>
2023-05-22 23:00:08 +0300

    VR-82 Add main menu stub

---
+++ Assets/Scripts/Menu/LevelChooseController.cs
@@ -0,0 +1,20 @@
+using System;
+using UI;
+using UnityEngine;
+
+namespace Menu {
+    public class LevelChooseController : MonoBehaviour {
+        public MenuController menuController;
+        public UiController uiController;
+
+        public void Start() {
+
+        }
+
+        public void Update() {
+
+        }
+
+
+    }
+}
