dc8ef460b23442cc3d6d3654d3c2e9b006482723
Pavel Solomatin <crave_ozer_man@mail.ru>
2023-05-22 19:17:38 +0300

    (fix) repair UiEventType enum

--- Assets/Scripts/Ui/UiEventType.cs
+++ Assets/Scripts/Ui/UiEventType.cs
@@ -2,10 +2,12 @@ namespace UI {
     public enum UiEventType : int {
         WinButtonClick = 0,
         LoseButtonClick = 1,
-        RestartButtonClick = 2,
-        MusicToggleClick = 3,
-        SoundToggleClick = 4,
-        HintButtonClick = 5,
-        QuitGameButtonClick = 6
+        QuitLevelButtonClick = 3,
+        NextLevelButtonClick = 4,
+        RestartButtonClick = 5,
+        MusicToggleClick = 6,
+        SoundToggleClick = 7,
+        HintButtonClick = 8,
+        QuitGameButtonClick = 9
     }
 }
